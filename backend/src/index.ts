d9a4bc6fb0f71e23de8c6bf2a1a1aae5da0ec202
WendersonBarros <wendercesar2@gmail.com>
2025-09-11 22:17:59 -0400

    feat: setup `Fastify` server with basic route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,24 @@
+import { request } from "http";
 import { AppDataSource } from "./data-source"
 import Fastify from "fastify"
 
 AppDataSource.initialize().then(async () => {
   console.log("Here you can setup and run express / fastify / any other framework.")
+
+  const fastify = Fastify({
+    logger: true
+  });
+
+  fastify.get('/', (request, reply) => {
+    reply.send({ hello: "world" });
+  });
+
+  fastify.listen({port: 3000}, (err, address) => {
+    if(err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+
+    console.log(`Server is listening on ${address}`);
+  });
 }).catch(error => console.log(error))
