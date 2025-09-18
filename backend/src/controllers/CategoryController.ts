124d5bd60c47c70f6740a6e2adbdf5005915a05d
WendersonBarros <wendercesar2@gmail.com>
2025-09-17 21:30:06 -0400

    feat: add `getOne` method to `CategoryController`

--- backend/src/controllers/CategoryController.ts
+++ backend/src/controllers/CategoryController.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { CategoryService } from "../services/CategoryService";
+import { BadRequestError, NotFoundError } from "../utils/errors";
 
 const categoryService = new CategoryService();
 
@@ -31,6 +32,35 @@ export class CategoryController {
       return reply.status(500).send({
         error: err.message
       });
+    }
+  }
+
+  static async getOne(request: FastifyRequest<{ Params: { id: number } }>, reply: FastifyReply) {
+    try {
+      const id = request.params.id;
+      
+      if (!Number.isInteger(Number(id))) {
+        throw new BadRequestError("Invalid id, must be a number");
+      }
+
+      const category = await categoryService.getCategoryById(id);
+      return reply.send(category);
+    } catch (err: any) {
+      if (err instanceof NotFoundError) {
+        return reply.status(404).send({ 
+          error: err.message 
+        });
+      }
+
+      if (err instanceof BadRequestError) {
+        return reply.status(400).send({ 
+          error: err.message
+        });
+      }
+
+      return reply.status(500).send({
+        error: "Internal server error"
+      })
     }
   }
 };
