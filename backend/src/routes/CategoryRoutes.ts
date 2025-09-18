22c38388a2c33f8f4b69391a6640f9a2dd96b431
WendersonBarros <wendercesar2@gmail.com>
2025-09-17 23:49:18 -0400

    feat: add PATCH /:id endpoint to `CategoryRoutes`

--- backend/src/routes/CategoryRoutes.ts
+++ backend/src/routes/CategoryRoutes.ts
@@ -5,4 +5,5 @@ export default async function CategoryRoutes(fastify: FastifyInstance) {
   fastify.post("/", CategoryController.create);
   fastify.get("/", CategoryController.getAll);
   fastify.get("/:id", CategoryController.getOne);
+  fastify.patch("/:id", CategoryController.update);
 };
