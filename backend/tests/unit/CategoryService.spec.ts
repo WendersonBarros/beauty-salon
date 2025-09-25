e6b215ce63425b93351c8de86a1e91e25c509542
WendersonBarros <wendercesar2@gmail.com>
2025-09-24 23:47:26 -0400

    test: add duplicate category check in `CategoryService.createCategory`

--- backend/tests/unit/CategoryService.spec.ts
+++ backend/tests/unit/CategoryService.spec.ts
@@ -33,4 +33,38 @@ describe("Category Service", () => {
       expect(result).toEqual({ id: 1, name: "Category test 1" });
     }
   );
+
+  test(
+    "It should not be possible to create a category with a repeated name",
+    async () => {
+      mockRepo.findOne.mockResolvedValue(null);
+      mockRepo.save.mockResolvedValue({
+        id: 2,
+        name: "Category with a repeated name"
+      });
+
+      const category1 = await categoryService.createCategory(
+        "Category with a repeated name"
+      );
+
+      expect(mockRepo.findOne).toHaveBeenCalledWith({
+        where: { name: "Category with a repeated name" }
+      });
+      expect(mockRepo.save).toHaveBeenCalled();
+      expect(category1).toEqual({
+        id: 2,
+        name: "Category with a repeated name"
+      });
+
+      mockRepo.findOne.mockResolvedValue({
+        id: 2,
+        name: "Category with a repeated name"
+      });
+
+      await expect(
+        categoryService.createCategory("Category with a repeated name")
+      ).rejects.toThrow(ConflictError);
+      expect(mockRepo.save).toHaveBeenCalledTimes(1); // Category1
+    }
+  );
 });
