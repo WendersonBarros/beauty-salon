802101b9121fef84ffb7c37e55341474ad1691be
WendersonBarros <wendercesar2@gmail.com>
2025-09-30 21:13:41 -0400

    feat: add GET / endpoint to `ProductRoutes`

--- backend/src/routes/ProductRoutes.ts
+++ backend/src/routes/ProductRoutes.ts
@@ -3,4 +3,5 @@ import { ProductController } from "../controllers/ProductController";
 
 export default async function ProductRoutes(fastify: FastifyInstance) {
   fastify.post("/", ProductController.create);
+  fastify.get("/", ProductController.getAll);
 };
