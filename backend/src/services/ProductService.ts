f3f8d2e3d6d3871fc2fb49db465800975ebc3ccc
WendersonBarros <wendercesar2@gmail.com>
2025-09-30 20:39:54 -0400

    feat: add `ProductService` with `createProduct` method

---
+++ backend/src/services/ProductService.ts
@@ -0,0 +1,38 @@
+import { Repository } from "typeorm";
+import { Product } from "../entity/Product";
+import { Category } from "../entity/Category";
+import { ConflictError, NotFoundError } from "../utils/errors";
+
+export class ProductService {
+  constructor(
+    private productRepo: Repository<Product>,
+    private categoryRepo: Repository<Category>
+  ) { };
+
+  async createProduct(name: string, price: number, categoryId: number) {
+    const category = await this.categoryRepo.findOne({
+      where: { id: categoryId }
+    });
+
+    if (!category) {
+      throw new NotFoundError(`Category with id ${categoryId} not found`);
+    }
+
+    const existing = await this.productRepo.findOne({
+      where: { name, category }
+    });
+
+    if (existing) {
+      throw new ConflictError(
+        "Product with the same name already exists inside this category"
+      );
+    }
+
+    const product = new Product();
+    product.name = name;
+    product.price = price;
+    product.category = category;
+
+    return this.productRepo.save(product);
+  };
+};
