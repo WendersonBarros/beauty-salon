c77d9b4abc08bb5479627a36af436f9a2fe6f8b8
WendersonBarros <wendercesar2@gmail.com>
2025-10-09 21:40:16 -0400

    feat: add `Gallery` component

---
+++ frontend/src/pages/Home/Gallery.tsx
@@ -0,0 +1,25 @@
+import nail1 from "../../assets/nail1.jpg";
+import nail2 from "../../assets/nail2.jpg";
+import nail3 from "../../assets/nail3.jpg";
+import nail4 from "../../assets/nail4.jpg";
+import nail5 from "../../assets/nail5.jpg";
+import nail6 from "../../assets/nail6.jpg";
+import Card from "../../components/Card";
+
+function Gallery() {
+  const images = [nail1, nail2, nail3, nail4, nail5, nail6];
+
+  return (
+    <section className="w-full">
+      <div className="flex justify-between flex-wrap gap-y-4 gap-x-2">
+        {images.map((image, idx) => {
+          return <Card key={idx} image={image} styles="w-60 h-60 mx-auto" />
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Gallery;
+
+
