15b4180f17b7e73b408b047ebcbdab27c353de05
WendersonBarros <wendercesar2@gmail.com>
2025-10-10 01:05:54 -0400

    feat: add `Reviews` section inside `Home`

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import salonPhoto from "../../assets/salon.jpg";
 import About from "./About";
 import Gallery from "./Gallery.tsx";
+import Reviews from "./Reviews.tsx";
 
 function Home() {
   return (
-    <main className="flex flex-col  items-center bg-black min-h-[80%] w-full text-white px-2 gap-4" >
+    <main
+      className="flex flex-col  items-center bg-black min-h-[80%] w-full
+      text-white px-2 gap-4 overflow-x-hidden"
+    >
       <figure className="w-full flex justify-center max-w-4xl m-auto">
         <img
           src={salonPhoto}
@@ -14,6 +18,7 @@ function Home() {
       </figure>
       <About />
       <Gallery />
+      <Reviews />
     </main>
   )
 }
