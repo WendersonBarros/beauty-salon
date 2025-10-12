c801f852faeaaf7afe08ffcef16d2f1503385761
WendersonBarros <wendercesar2@gmail.com>
2025-10-11 22:25:52 -0400

    chore: add `react-router-dom` and configure routing in `App` component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
