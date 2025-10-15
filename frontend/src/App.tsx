a7f57889ab3102ad47bc874b8893ef37b94ca20a
WendersonBarros <wendercesar2@gmail.com>
2025-10-15 00:19:07 -0400

    feat: add `QueryClientProvider` and configure `React Query` in `App`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,27 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home/Home'
 import Prices from './pages/Prices/Prices';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/prices" element={<Prices />} />
-      </Routes>
-      <Footer />
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/prices" element={<Prices />} />
+        </Routes>
+        <Footer />
+      </QueryClientProvider>
     </BrowserRouter>
   )
 }
