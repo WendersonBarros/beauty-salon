c651bbb7d9b02d7dd3cf03c52e9e0519da474ea0
WendersonBarros <wendercesar2@gmail.com>
2025-10-19 23:54:59 -0400

    feat: add admin login logic inside `AdminService.ts`

---
+++ backend/src/services/AdminService.ts
@@ -0,0 +1,34 @@
+import { Repository } from "typeorm";
+import { Admin } from "../entity/Admin";
+import { UnauthorizedError } from "../utils/errors";
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export class AdminService {
+  constructor(private adminRepo: Repository<Admin>) { }
+
+  async adminLogin(login: string, password: string) {
+    const admin = await this.adminRepo.findOne({
+      where: { login }
+    });
+
+    if (!admin) {
+      throw new UnauthorizedError("Invalid login or password");
+    }
+
+    const isCorrectPassword = await bcrypt.compare(password, admin.password);
+    if (!isCorrectPassword) {
+      throw new UnauthorizedError("Invalid login or password");
+    }
+
+    const token = jwt.sign(
+      { id: admin.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    return { token }
+  }
+}
