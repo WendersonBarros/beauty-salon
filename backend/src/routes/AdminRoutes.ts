5137ca8058333ad1a3bfe0802f7b836069009456
WendersonBarros <wendercesar2@gmail.com>
2025-11-14 01:30:28 -0400

    change: modify the login route name to `/login/

--- backend/src/routes/AdminRoutes.ts
+++ backend/src/routes/AdminRoutes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from "fastify";
 import { AdminController } from "../controllers/AdminController";
 
 export default async function AdminRoutes(fastify: FastifyInstance) {
-  fastify.post("/", AdminController.login);
+  fastify.post("/login", AdminController.login);
 };
