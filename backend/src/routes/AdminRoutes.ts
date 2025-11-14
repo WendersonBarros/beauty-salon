d67ce035a1f9e400c7664b148687bfded71bb5f6
WendersonBarros <wendercesar2@gmail.com>
2025-11-14 01:31:56 -0400

    feat: add token refresh route inside `AdminRountes.ts`

--- backend/src/routes/AdminRoutes.ts
+++ backend/src/routes/AdminRoutes.ts
@@ -3,4 +3,5 @@ import { AdminController } from "../controllers/AdminController";
 
 export default async function AdminRoutes(fastify: FastifyInstance) {
   fastify.post("/login", AdminController.login);
+  fastify.post("/refreshtoken", AdminController.refreshAcessToken);
 };
