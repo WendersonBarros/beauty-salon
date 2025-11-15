e683150e394352bd770ed27ddfb2b97e2b4a7fd4
WendersonBarros <wendercesar2@gmail.com>
2025-11-14 23:48:41 -0400

    fix: add missing await in `AdminController.ts`

--- backend/src/controllers/AdminController.ts
+++ backend/src/controllers/AdminController.ts
@@ -42,7 +42,7 @@ export class AdminController {
     }
 
     try {
-      const newAcessToken = adminService
+      const newAcessToken = await adminService
         .generateAccessTokenFromRefreshToken(refreshTokenValue);
 
       reply
