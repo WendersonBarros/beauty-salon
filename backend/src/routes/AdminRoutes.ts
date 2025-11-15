9eeb84876a2d39ff06c65c4379b1c0c58457f543
WendersonBarros <wendercesar2@gmail.com>
2025-11-15 00:03:33 -0400

    feat: add verify route in `AdminRoutes.ts` protected by `authMiddleware`

--- backend/src/routes/AdminRoutes.ts
+++ backend/src/routes/AdminRoutes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { AdminController } from "../controllers/AdminController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 export default async function AdminRoutes(fastify: FastifyInstance) {
   fastify.post("/login", AdminController.login);
   fastify.post("/refreshtoken", AdminController.refreshAcessToken);
+  fastify.get("/verify", { preHandler: authMiddleware }, (_req, reply) => {
+    reply.status(200).send({ ok: true });
+  });
 };
