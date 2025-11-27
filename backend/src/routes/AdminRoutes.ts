424475381b2dc64aab9fadbc301467f713d6bbce
WendersonBarros <wendercesar2@gmail.com>
2025-11-27 02:51:31 -0400

    feat: update verify route response to return the logged admin in `AdminRoutes.ts`

--- backend/src/routes/AdminRoutes.ts
+++ backend/src/routes/AdminRoutes.ts
@@ -5,7 +5,9 @@ import { authMiddleware } from "../middlewares/authMiddleware";
 export default async function AdminRoutes(fastify: FastifyInstance) {
   fastify.post("/login", AdminController.login);
   fastify.post("/refreshtoken", AdminController.refreshAcessToken);
-  fastify.get("/verify", { preHandler: authMiddleware }, (_req, reply) => {
-    reply.status(200).send({ ok: true });
+  fastify.get("/verify", { preHandler: authMiddleware }, (req, reply) => {
+    reply.status(200).send({
+      user: req.user.login
+    });
   });
 };
