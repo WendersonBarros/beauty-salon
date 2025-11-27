02df291feadca5c83968d68807da885a56431971
WendersonBarros <wendercesar2@gmail.com>
2025-11-27 02:59:46 -0400

    refactor: move `QueryClient` setup from `App.tsx` to `queryClient.ts`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,47 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home/Home'
 import Prices from './pages/Prices/Prices';
 import Login from './pages/Admin/Login';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-    },
-  },
-});
+import { queryClient } from './api/queryClient';
+import RedirectIfLogged from './auth/RedirectIfLogged';
+import ProtectedRoute from './auth/ProtectRoute';
+import AuthProvider from './auth/AuthProvider';
 
 function App() {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/prices" element={<Prices />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
+        <AuthProvider>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/prices" element={<Prices />} />
+
+            <Route
+              path="/login"
+              element={
+                <RedirectIfLogged>
+                  <Login />
+                </RedirectIfLogged>
+              }
+            />
+
+            <Route
+              path="/admin"
+              element={
+                <ProtectedRoute>
+                  <h1 className='text-red-400'>ADMIN PAGE</h1>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Footer />
+        </AuthProvider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </BrowserRouter >
   )
 }
 
