86de525e9e8a37af09fedb4a9d82a09bc18f4ef7
WendersonBarros <wendercesar2@gmail.com>
2025-11-27 21:54:45 -0400

    feat: update `Login.tsx` to integrate new auth flow

--- frontend/src/pages/Admin/Login.tsx
+++ frontend/src/pages/Admin/Login.tsx
@@ -1,15 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useAuth } from "../../auth/useAuth";
+import api from "../../api/authApi";
+import axios from "axios";
+import { alertError } from "../../utils/sweetalert";
 
 function Login() {
-  const [login, setLogin] = useState("");
+  const { login } = useAuth();
+  const [userLogin, setUserLogin] = useState("");
   const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    // if token is valid
-    // redirect to admin panel
-    // if not try to use the refresh token
-    //
-  }, []);
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      const res = await api.post("/admin/login", {
+        login: userLogin,
+        password,
+      });
+
+      const token = res.data.acessToken;
+      login(token);
+    } catch (error: unknown) {
+      console.error(error);
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          alertError("Credenciais incorretas, tente novamente");
+          return;
+        }
+
+        alertError("Houve um erro inesperado.");
+        return;
+      }
+
+      alertError("Houve um erro inesperado.");
+    }
+  }
 
   return (
     <main className="grid px-2 gap-2">
@@ -19,24 +44,31 @@ function Login() {
         </h2>
       </div>
 
-      <form className="mt-7 min-h-screen flex flex-col gap-4">
+      <form
+        onSubmit={handleSubmit}
+        className="mt-7 min-h-screen flex flex-col gap-4"
+      >
         <div>
-          <label htmlFor="login" className="font-medium text-white ml-2">Login</label>
+          <label htmlFor="login" className="font-medium text-white ml-2">
+            Login
+          </label>
           <div className="mt-2">
             <input
               type="text"
               id="login"
-              value={login}
-              onChange={event => setLogin(event.target.value)}
+              value={userLogin}
+              onChange={event => setUserLogin(event.target.value)}
               autoComplete="username"
-              className="w-full p-4  text-sm text-black border-2
-            rounded-lg bg-gray-50 focus:outline-none focus:border-darkYellow"
+              className="w-full p-4  text-sm text-black border-2 rounded-lg
+              bg-gray-50 focus:outline-none focus:border-darkYellow"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="password" className="font-medium text-white ml-2">Senha</label>
+          <label htmlFor="password" className="font-medium text-white ml-2">
+            Senha
+          </label>
           <div className="mt-2">
             <input
               type="password"
